8cbeb312259d7a1c216f4580ad19fd1fa21834ee
ruthgabrielle <ruthfgms@gmail.com>
2021-09-26 16:08:19 -0300

    final fix

--- src/services/server.js
+++ src/services/server.js
@@ -1,21 +1,4 @@
 import { Server, Model } from "miragejs";
-var cors_proxy = require("cors-anywhere");
-
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || "127.0.0.1";
-
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 3000;
-
-cors_proxy
-  .createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"],
-  })
-  .listen(port, host, function () {
-    console.log("Running CORS Anywhere on " + host + ":" + port);
-  });
 
 export function makeServer({ environment = "development" } = {}) {
   let server = new Server({
