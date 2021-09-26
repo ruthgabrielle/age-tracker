5b830e35d23de962d5a00f0a7fa044a6810d0fa7
ruthgabrielle <ruthfgms@gmail.com>
2021-09-26 12:34:41 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { makeServer } from './services/server';
 import {InfoProvider } from './hooks/InfoContext';
 import Infos from './pages/infos';
 
-makeServer()
 
 export function App() {
+  
+  makeServer()
+
   return (
     <InfoProvider>
      <Infos />
