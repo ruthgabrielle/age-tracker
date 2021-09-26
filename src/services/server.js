c486d52521e9343f8ba972b5337ec494417dea5a
ruthgabrielle <ruthfgms@gmail.com>
2021-09-26 16:02:15 -0300

    try solve cors problem

--- src/services/server.js
+++ src/services/server.js
@@ -1,35 +1,52 @@
-import { Server, Model } from 'miragejs'
+import { Server, Model } from "miragejs";
+var cors_proxy = require("cors-anywhere");
 
-export function makeServer({ environment = "development" } = {}){
-let server = new Server({
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || "127.0.0.1";
+
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 3000;
+
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
+
+export function makeServer({ environment = "development" } = {}) {
+  let server = new Server({
     environment,
-    
-  models: {
-    info: Model
-  },
 
-  seeds(server){
-    server.db.loadData({
-      infos: [
-        { "id": "1", "firstName": "Ruth", "age": 22 },
-        { "id": "2", "firstName": "Sarah", "age": 25 },
-        { "id": "3", "firstName": "Artur", "age": 30 }
-      ]
-    })
-  },
+    models: {
+      info: Model,
+    },
 
-    routes(){
-        this.namespace = 'api'
+    seeds(server) {
+      server.db.loadData({
+        infos: [
+          { id: "1", firstName: "Ruth", age: 22 },
+          { id: "2", firstName: "Sarah", age: 25 },
+          { id: "3", firstName: "Artur", age: 30 },
+        ],
+      });
+    },
 
-        this.get('/infos', () => {
-            return this.schema.all('info')
-        })
+    routes() {
+      this.namespace = "api";
 
-        this.post('/infos', (schema, request) => {
-            const data = JSON.parse(request.requestBody)
-            return schema.create('info', data)
-          })
-    }
-})
-return server
+      this.get("/infos", () => {
+        return this.schema.all("info");
+      });
+
+      this.post("/infos", (schema, request) => {
+        const data = JSON.parse(request.requestBody);
+        return schema.create("info", data);
+      });
+    },
+  });
+  return server;
 }
